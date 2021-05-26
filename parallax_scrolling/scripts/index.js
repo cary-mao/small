20b2d562cd815a24abef210a2a492d4e086ff07a
cary-mao <cary_mao@163.com>
2021-05-26 19:43:36 +0800

    feat(parallax_scrolling): new demo

---
+++ parallax_scrolling/scripts/index.js
@@ -0,0 +1,20 @@
+const moon = document.getElementById('moon')
+const mountain = document.getElementById('mountain')
+const road = document.getElementById('road')
+const text = document.getElementById('text')
+
+
+window.addEventListener('scroll', () => {
+  const value = window.scrollY
+
+  /**
+   * 核心在于每个对象都是top: 0, left: 0
+   * 即使是text，也是通过flex定位的，再设置为relative，相对于自身定位
+   * 所以，这里设置top和left时通过value的倍数来定，就相当于对象移动了value的倍数长度。
+   */
+  bg.style.top = value * 0.5 + 'px'
+  moon.style.left = -value * 0.5 + 'px'
+  mountain.style.top = -value * 0.15 + 'px';
+  road.style.top = value * 0.15 + 'px';
+  text.style.top = value * 1 + 'px';
+})
